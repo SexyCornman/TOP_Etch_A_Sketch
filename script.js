56a42feab3498f5a285f5007e186abf5dcbfdce0
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-09-29 14:39:28 +0700

    manually created 16*16 grid to see how it look like.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
 
 
-function clearGrid() {
+function createGrid() {
     for(i=0; i < allGridCell.length; i++){
     allGridCell[i].style.backgroundColor ='yellow';
     }
 }
-
+const div = document.createElement('div');
 const gridContainer = document.querySelector('#grid-container');
 const allGridCell = document.querySelectorAll('#grid-cell');
 const column = document.querySelectorAll('.column');
+
+
+// testing
+// gridContainer.append(div)
+// div.classList.add('column')
+
+
+
+
+
+
