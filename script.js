45ff14f9439024ab1bf02711f3735f623470cede
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-10-02 16:04:22 +0700

    Trying other method of generating grid

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 
 
 
-function createGrid() {
-    for(i=0; i < allGridCell.length; i++){
-    allGridCell[i].style.backgroundColor ='yellow';
-    }
-}
+// function createGrid() {
+//     for(i=0; i < allGridCell.length; i++){
+//     allGridCell[i].style.backgroundColor ='yellow';
+//     }
+// }
 
 const gridContainer = document.querySelector('#grid-container');
 const allGridCell = document.querySelectorAll('#grid-cell');
-const column = document.querySelectorAll('#column');
+const column = document.querySelectorAll;('#column');
 
 
 // testing
-function createColumn(gridNum){    
+function createGrid(gridNum){
+    for(i=0; i < gridNum;i++){
+    const griddiv = document.createElement('div');
+    column[i].append(griddiv)
+    griddiv.setAttribute('id','grid-cell');
+    griddiv.classList.add('gridcell');   
+
+}
+}
+
+function createColumn(gridNum){   
+    let n = gridNum; 
     for(i=0; i < gridNum;i++){
     const div = document.createElement('div');
     gridContainer.append(div)
     div.setAttribute('id','column');               
-            
+
     }
 }
+
 
 
 
