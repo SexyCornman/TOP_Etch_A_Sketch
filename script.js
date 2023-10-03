b2b42a2c62fea3d87a6c13e627f7456548561d81
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-10-03 15:44:05 +0700

    change appenChild to append

--- script.js
+++ script.js
@@ -13,16 +13,32 @@ function createGrid(gridNum){
     for(i=0; i < gridNum;i++){
     const row  = document.createElement('div');
     row.setAttribute('id','column');   
-    gridContainer.appendChild(row);   
+    gridContainer.append(row);   
 
         for(j=0; j < gridNum;j++){
         const pixel = document.createElement('div');
         pixel.classList.add('gridcell');
-        row.appendChild(pixel);
+        row.append(pixel);
     }                    
 
     }
 }
 
+function gridReset() {
+    gridContainer.innerHTML ="";
+    createGrid(16);
+}
+
+function changeGridSize() {
+    let gridSize = prompt ("please select grid size");
+    if (gridSize > 0 && gridSize <=100){
+        gridContainer.innerHTML ="";
+        createGrid(gridSize);
+    }
+    else alert("nah that wont work");
+}
 
 
+
+
+createGrid(16);
